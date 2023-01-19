c8480897ca742a9fa6956982dd6146e3cc4aa71c
Gleb Kosyrev <fakemirror11@gmail.com>
2023-01-19 11:46:23 +0900

    Update aligned_dataset.py

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -5,10 +5,14 @@ from PIL import Image
 import numpy as np
 
 class AlignedDataset(BaseDataset):
-    """A dataset class for paired image dataset.
+    """
+    This dataset class can load aligned pairs of images.
 
-    It assumes that the directory '/path/to/data/train' contains image pairs in the form of {A,B}.
-    During test time, you need to prepare a directory '/path/to/data/test'.
+    It requires two directories to host training images from domain A '/path/to/data/trainA'
+    and from domain B '/path/to/data/trainB' respectively.
+    You can train the model with the dataset flag '--dataroot /path/to/data'.
+    Similarly, you need to prepare two directories:
+    '/path/to/data/testA' and '/path/to/data/testB' during test time.
     """
 
     def __init__(self, opt):
@@ -18,52 +22,49 @@ class AlignedDataset(BaseDataset):
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         BaseDataset.__init__(self, opt)
-        # self.dir_AB_eye = os.path.join(opt.dataroot_eye, opt.phase)  # get the image directory
-        # self.dir_AB_mouth = os.path.join(opt.dataroot_mouth, opt.phase)  # get the image directory
-        self.dir_AB = os.path.join(opt.dataroot, opt.phase)  # get the image directory
+        self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')  # create a path '/path/to/data/trainA'
+        self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')  # create a path '/path/to/data/trainB'
+        
 
-        # self.AB_paths_eye = sorted(make_dataset(self.dir_AB_eye, opt.max_dataset_size))  # get image paths
-        # self.AB_paths_mouth = sorted(make_dataset(self.dir_AB_mouth, opt.max_dataset_size))  # get image paths
-        self.AB_paths = sorted(make_dataset(self.dir_AB, opt.max_dataset_size))  # get image paths
-
-
-        assert(self.opt.load_size >= self.opt.crop_size)   # crop_size should be smaller than the size of loaded image
-        self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
-        self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
+        self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))   # load images from '/path/to/data/trainA'
+        self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))    # load images from '/path/to/data/trainB'
+        self.A_size = len(self.A_paths)  # get the size of dataset A
+        self.B_size = len(self.B_paths)  # get the size of dataset B
+        
+        assert self.A_size == self.B_size, "unaligned dataset"
+        
+        btoA = self.opt.direction == 'BtoA'
+        input_nc = self.opt.output_nc if btoA else self.opt.input_nc       # get the number of channels of input image
+        output_nc = self.opt.input_nc if btoA else self.opt.output_nc      # get the number of channels of output image
+        self.transform_A = get_transform(self.opt, grayscale=(input_nc == 1))
+        self.transform_B = get_transform(self.opt, grayscale=(output_nc == 1))
 
     def __getitem__(self, index):
         """Return a data point and its metadata information.
 
         Parameters:
-            index - - a random integer for data indexing
+            index (int)      -- a random integer for data indexing
 
         Returns a dictionary that contains A, B, A_paths and B_paths
-            A (tensor) - - an image in the input domain
-            B (tensor) - - its corresponding image in the target domain
-            A_paths (str) - - image paths
-            B_paths (str) - - image paths (same as A_paths)
+            A (tensor)       -- an image in the input domain
+            B (tensor)       -- its corresponding image in the target domain
+            A_paths (str)    -- image paths
+            B_paths (str)    -- image paths
         """
-        # read a image given a random integer index
-        AB_path = self.AB_paths[index]
-        AB = Image.open(AB_path).convert('RGBA')
-        # split AB image into A and B
-        w, h = AB.size
-        w2 = int(w / 2)
-        A = AB.crop((0, 0, w2, h))
-        B = AB.crop((w2, 0, w, h))
-        A_im = np.array(A)
-        B_im = np.array(B)
-        # apply the same transform to both A and B
-        transform_params = get_params(self.opt, A.size)
-        A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
-        B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
+        A_path = self.A_paths[index % self.A_size]  # make sure index is within then range
+        B_path = self.B_paths[index % self.B_size]
+        A_img = Image.open(A_path).convert('RGBA')
+        B_img = Image.open(B_path).convert('RGBA')
+        # apply image transformation
+        A = self.transform_A(A_img)
+        B = self.transform_B(B_img)
 
-        A = A_transform(A)
-        B = B_transform(B)
-
-
-        return {'A': A, 'B': B, 'A_paths': AB_path, 'B_paths': AB_path, 'A_im': A_im, 'B_im': B_im}
+        return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
 
     def __len__(self):
-        """Return the total number of images in the dataset."""
-        return len(self.AB_paths)
+        """Return the total number of images in the dataset.
+
+        As we have two datasets with potentially different number of images,
+        we take a maximum of
+        """
+        return max(self.A_size, self.B_size)
